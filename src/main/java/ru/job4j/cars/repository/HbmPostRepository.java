4153938994b550d18809b1e9c22fb8b030e1b3e2
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-08-23 22:40:40 +0300

    1. Фильтры для объявлений машин fix

--- src/main/java/ru/job4j/cars/repository/HbmPostRepository.java
+++ src/main/java/ru/job4j/cars/repository/HbmPostRepository.java
@@ -2,13 +2,10 @@ package ru.job4j.cars.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import ru.job4j.cars.model.Car;
-import ru.job4j.cars.model.Model;
 import ru.job4j.cars.model.Post;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,52 +15,37 @@ public class HbmPostRepository implements PostRepository {
 
     @Override
     public List<Post> findAll() {
-        return crudRepository.query("from Post p JOIN FETCH p.user", Post.class);
+        return crudRepository.query("FROM Post p JOIN FETCH p.user", Post.class);
     }
 
     @Override
     public List<Post> findAllWithPhoto() {
-        return crudRepository.tx(session -> {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<Post> cq = cb.createQuery(Post.class);
-            Root<Post> post = cq.from(Post.class);
-            post.fetch("user");
-            post.join("images", JoinType.LEFT);
-            Predicate hasImages = cb.isNotEmpty(post.get("images"));
-            cq.select(post).where(hasImages);
-            TypedQuery<Post> query = session.createQuery(cq);
-            return query.getResultList();
-        });
+        return crudRepository.query("""
+                FROM Post p
+                JOIN FETCH p.user
+                LEFT JOIN FETCH p.images i
+                WHERE SIZE(i) > 0
+        """, Post.class);
     }
 
     @Override
     public List<Post> findAllByLastDay() {
-        return crudRepository.tx(session -> {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<Post> cq = cb.createQuery(Post.class);
-            Root<Post> post = cq.from(Post.class);
-            post.fetch("user");
-            LocalDateTime lastDay = LocalDateTime.now().minusDays(1);
-            Predicate lastDayPredicate = cb.greaterThanOrEqualTo(post.get("created"), lastDay);
-            cq.select(post).where(lastDayPredicate);
-            TypedQuery<Post> query = session.createQuery(cq);
-            return query.getResultList();
-        });
+        LocalDateTime lastDay = LocalDateTime.now().minusDays(1);
+        return crudRepository.query("""
+                FROM Post p
+                JOIN FETCH p.user
+                WHERE p.created > :lastDay
+        """, Post.class, Map.of("lastDay", lastDay));
     }
 
     @Override
     public List<Post> findAllByModel(String model) {
-        return crudRepository.tx(session -> {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<Post> cq = cb.createQuery(Post.class);
-            Root<Post> post = cq.from(Post.class);
-            post.fetch("user");
-            Join<Post, Car> carJoin = post.join("car");
-            Join<Car, Model> modelJoin = carJoin.join("model");
-            Predicate modelPredicate = cb.equal(modelJoin.get("name"), model);
-            cq.select(post).where(modelPredicate);
-            TypedQuery<Post> query = session.createQuery(cq);
-            return query.getResultList();
-        });
+        return crudRepository.query("""
+                FROM Post p
+                JOIN FETCH p.user
+                JOIN FETCH p.car c
+                JOIN FETCH c.model m
+                WHERE m.name=:model
+        """, Post.class, Map.of("model", model));
     }
 }
